5733c264d71c7155bfe1337bb9d0aab8595284ca
Filip Kin <filip@kinmails.com>
2021-03-08 16:45:55 -0500

    joj

--- resources/scripts/components/server/backups/BackupContainer.tsx
+++ resources/scripts/components/server/backups/BackupContainer.tsx
@@ -1,80 +1,52 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Spinner from '@/components/elements/Spinner';
-import useFlash from '@/plugins/useFlash';
-import Can from '@/components/elements/Can';
-import CreateBackupButton from '@/components/server/backups/CreateBackupButton';
+import React, { useState } from 'react';
 import FlashMessageRender from '@/components/FlashMessageRender';
-import BackupRow from '@/components/server/backups/BackupRow';
 import tw from 'twin.macro';
-import getServerBackups, { Context as ServerBackupContext } from '@/api/swr/getServerBackups';
 import { ServerContext } from '@/state/server';
 import ServerContentBlock from '@/components/elements/ServerContentBlock';
-import Pagination from '@/components/elements/Pagination';
+import { Context as ServerBackupContext } from '@/api/swr/getServerBackups';
+import Select from '@/components/elements/Select';
+import Label from '@/components/elements/Label';
+import TitledGreyBox from '@/components/elements/TitledGreyBox';
+import useSWR from 'swr'
 
+const fetcher = (url: string): Promise<any> => {
+    return fetch(url).then(res => res.json());
+}
+  
 const BackupContainer = () => {
-    const { page, setPage } = useContext(ServerBackupContext);
-    const { clearFlashes, clearAndAddHttpError } = useFlash();
-    const { data: backups, error, isValidating } = getServerBackups();
-
-    const backupLimit = ServerContext.useStoreState(state => state.server.data!.featureLimits.backups);
-
-    useEffect(() => {
-        if (!error) {
-            clearFlashes('backups');
-
-            return;
-        }
-
-        clearAndAddHttpError({ error, key: 'backups' });
-    }, [ error ]);
-
-    if (!backups || (error && isValidating)) {
-        return <Spinner size={'large'} centered/>;
+    const uuid = ServerContext.useStoreState(state => state.server.data!.uuid);
+    const [selected, setSelected] = useState('latest');
+    
+    const handleChange = (evt) => {
+        setSelected(evt.currentTarget.value);
     }
+
+    const { data: versions } = useSWR('https://backups-api.kiwatech.net/', fetcher);
+    const { data: folder } = useSWR(`https://backups-api.kiwatech.net/${encodeURIComponent(selected)}/${uuid}/`, fetcher);
 
     return (
         <ServerContentBlock title={'Backups'}>
-            <FlashMessageRender byKey={'backups'} css={tw`mb-4`}/>
-            <Pagination data={backups} onPageSelect={setPage}>
-                {({ items }) => (
-                    !items.length ?
-                        // Don't show any error messages if the server has no backups and the user cannot
-                        // create additional ones for the server.
-                        !backupLimit ?
-                            null
-                            :
-                            <p css={tw`text-center text-sm text-neutral-300`}>
-                                {page > 1 ?
-                                    'Looks like we\'ve run out of backups to show you, try going back a page.'
-                                    :
-                                    'It looks like there are no backups currently stored for this server.'
+            <FlashMessageRender byKey={'backups'} css={tw`mb-4`} />
+            <div css={tw`md:flex`}>
+                <div css={tw`w-full md:flex-1 md:mr-10`}>
+                    <TitledGreyBox title={'Restore a backup'} css={tw`mb-6 md:mb-10`}>
+                        <div>
+                            <Label>Versions</Label>
+                            <Select onChange={handleChange}>
+                                {
+                                    (!versions) ? 'loading' : versions.map(i => {
+                                        return <option value={i}>{new Date(i).toLocaleDateString()}</option>
+                                    })
                                 }
-                            </p>
-                        :
-                        items.map((backup, index) => <BackupRow
-                            key={backup.uuid}
-                            backup={backup}
-                            css={index > 0 ? tw`mt-2` : undefined}
-                        />)
-                )}
-            </Pagination>
-            {backupLimit === 0 &&
-            <p css={tw`text-center text-sm text-neutral-300`}>
-                Backups cannot be created for this server.
-            </p>
-            }
-            <Can action={'backup.create'}>
-                <div css={tw`mt-6 sm:flex items-center justify-end`}>
-                    {(backupLimit > 0 && backups.items.length > 0) &&
-                    <p css={tw`text-sm text-neutral-300 mb-4 sm:mr-6 sm:mb-0`}>
-                        {backups.items.length} of {backupLimit} backups have been created for this server.
-                    </p>
-                    }
-                    {backupLimit > 0 && backupLimit !== backups.items.length &&
-                    <CreateBackupButton css={tw`w-full sm:w-auto`}/>
-                    }
+                            </Select>
+                        </div>
+                        <div css={tw`mt-6`}>
+                            <Label>Files</Label>
+                            {(!folder) ? 'loading' : JSON.stringify(folder)}
+                        </div>
+                    </TitledGreyBox>
                 </div>
-            </Can>
+            </div>
         </ServerContentBlock>
     );
 };
